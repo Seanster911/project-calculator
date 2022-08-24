42f31cbcd58bf899c059e1b35710f0c9135d3d0a
seandavidmurphy1992 <seandavidmurphy1992@gmail.com>
2022-08-24 22:23:59 +0100

    all major fatures and options added.
    some extra credit been done, no backspace button, no keyboard support.
    may revisit at later date to add these features.

--- calculator.js
+++ calculator.js
@@ -7,27 +7,66 @@ const operators = document.getElementsByClassName("operators");
 let firstValue = 0;
 let secondValue = 0;
 let operatorRecord = "";
+let answered = false;
+let answerRecord = 0;
+
+function roundDecimals(answer) {
+    if(answer.split(".")[1].length > 15) {
+        return parseFloat(answer).toFixed(15);
+    }
+
+    return answer;
+}
 
 function operate() {
-    if (secondValue === 0) {
-        secondValue = display.value;
-    }
+    if (secondValue === 0 && answered === false) {
+        secondValue = parseFloat(display.value);
+    };
     
     if (operatorRecord === "+") {
-        display.value = firstValue + secondValue;
+        //needed parsefloat to prevent it concatinating strings
+        answerRecord = parseFloat(firstValue) + parseFloat(secondValue);
+        display.value = answerRecord;
+        answered = true;
+
+        if (display.value.includes(".")) {
+            display.value = roundDecimals(display.value);
+        };
     } else if (operatorRecord === "-") {
-        display.value = firstValue - secondValue;   
+        answerRecord = firstValue - secondValue; 
+        display.value = answerRecord;
+        answered = true;  
+
+        if (display.value.includes(".")) {
+            display.value = roundDecimals(display.value);
+        };
     } else if (operatorRecord === "*") {
-        display.value = firstValue * secondValue;
+        answerRecord = firstValue * secondValue;
+        display.value = answerRecord;
+        answered = true;
+        
+        if (display.value.includes(".")) {
+            display.value = roundDecimals(display.value);
+        };
     } else if (operatorRecord === "/") {
-        display.value = firstValue / secondValue;
-    }
+        answerRecord = firstValue / secondValue;
+        console.log(answerRecord);
+        display.value = answerRecord;
+        answered = true;
+
+        if (display.value.includes(".")) {
+            display.value = roundDecimals(display.value);
+        };
+
+
+        if (firstValue === 0 || secondValue === 0) {
+            display.value = "You can't divide by zero!";
+            return;
+        };
+    };
 
     firstValue = display.value;
 
-    console.log(firstValue);
-    console.log(secondValue);
-    console.log(operatorRecord);
 }
 
 //assigning event to equals button
@@ -39,6 +78,8 @@ clear.addEventListener("click", function(e) {
     display.value = 0;
     firstValue = 0;
     secondValue = 0;
+    operatorRecord = "";
+    answered = false;
 });
 
 
@@ -61,12 +102,27 @@ for (i = 0; i < allButtons.length; i++) {
 //assigning the events to the operators buttons
 for (i = 0; i < operators.length; i++) {
     operators[i].addEventListener("click", function(e) {
-        if (firstValue === 0) {
+        //making sure to collect initial value if screen has not been filled
+        if (firstValue === 0 && isNaN(display.textContent)) {
+            firstValue = 0;
+            display.value = 0;
+            operatorRecord = this.textContent;
+        } else if (firstValue === 0) {
             firstValue = parseFloat(display.value);
 
             display.value = 0;
 
             operatorRecord = this.textContent;
+        } else if (answered === true) {
+            firstValue = parseFloat(display.value);
+            operatorRecord = this.textContent;
+            display.value = 0; 
+            answered = false;
+            secondValue = 0; 
         }
+
+        console.log(firstValue);
+        console.log(operatorRecord);
+        console.log(secondValue);       
     });
 };
